54027aabf68617cdeda8f8e50b129cd013e0b690
Mohit <mohitver01@gmail.com>
2024-01-28 15:55:56 -0500

    vid updated

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -4,7 +4,7 @@ import * as classes from "./VideoPlayer.module.css"
 export const VideoPlayer = () => {
   return (
     <video className={classes.video} controls>
-      <source  src="placeholder.mp4" type="video/mp4" />
+      <source  src="appDemo.mp4" type="video/mp4" />
     </video>
   );
 };
